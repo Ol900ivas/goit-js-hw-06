3525e6b6e4b4f5ad16ef3b6905f7ecef05da74a9
Olga Lorenz <ol900ivas@gmail.com>
2023-02-02 05:38:58 +0200

    add task 10

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,34 @@
+// const inputEl = document.querySelector("#validation-input");
+// const rigthLength = Number(inputEl.getAttribute('data-length'));
+
+// inputEl.addEventListener('blur', (event) => {
+//     if (event.currentTarget.value.length === rigthLength) {
+//         inputEl.classList.add('valid');
+//         inputEl.classList.remove('invalid');
+//     } else {
+//         inputEl.classList.add('invalid');
+//         inputEl.classList.remove('valid');
+//     }
+// });
+
+// ============================
 const inputEl = document.querySelector("#validation-input");
 const rigthLength = Number(inputEl.getAttribute('data-length'));
 
 inputEl.addEventListener('blur', (event) => {
     if (event.currentTarget.value.length === rigthLength) {
-        inputEl.classList.add('valid');
-        inputEl.classList.remove('invalid');
+        changeClasses();
     } else {
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
+        changeClasses();
     }
 });
 
-
+function changeClasses(x, y) {
+    if (!inputEl.classList.contains("valid")) {
+        inputEl.classList.add("valid");
+        inputEl.classList.remove("invalid");
+    } 
+    elem.classList.replace("valid", "invalid")
+        // inputEl.classList.add("invalid");
+        // inputEl.classList.remove("valid");
+};
--- js/task-10.js
+++ js/task-10.js
@@ -1,52 +1,36 @@
-// 1. Напиши скрипт створення і очищення колекції елементів. 
-// Користувач вводить кількість елементів в input і натискає кнопку Створити,
-// після чого рендериться колекція.
-// Натисненням на кнопку Очистити, колекція елементів очищається.
-
-// 2. Створи функцію createBoxes(amount), яка приймає один параметр - число.
-//  Функція створює стільки < div >, скільки вказано в amount
-//   і додає їх у div#boxes.
-
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX.
-//  Використовуй готову функцію getRandomHexColor для отримання кольору.
-
-
-//  3. Створи функцію destroyBoxes(), яка очищає вміст div#boxes,
-//   у такий спосіб видаляючи всі створені елементи.
-
-const amount = document.querySelector('input').value;
+const amountEl = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const wrapEl = document.querySelector('#boxes');
 
-
-
-console.log(amount);
-console.log(createBtn);
-console.log(destroyBtn);
-console.log(wrapEl);
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+function getAmount() {
+  const amount = amountEl.value;
+  if (amountEl.value > 0) {
+    createBoxes(amount);
+  }
+};
 
 function createBoxes(amount) {
-
-  let side = 30;
-
-  // for (let i = 0, i <= amount, i += 1) {
-  //   const box = document.createElement('div');
-  // box.style.width = `${side}px`;
-  // box.style.height = `${side}px`;
-  // box.style.marginBottom = "10px";
-  // box.style.backgroundColor = "blue";
-  // wrapEl.append(box);
-
-  // side += 10;
-  // };
+  destroyBoxes();
   
+  let boxes = [];
+  let side = 30;
+  
+for (let i = 1; i <= amount; i += 1) {
+  const box = document.createElement('div');
+  box.style.width = `${side}px`;
+  box.style.height = `${side}px`;
+  box.style.marginBottom = "10px";
+  box.style.backgroundColor = `${getRandomHexColor()}`;
+  boxes.push(box);
+
+  side += 10;
+  }  
+  wrapEl.append(...boxes);
 };
   
 function destroyBoxes() {
@@ -54,6 +38,6 @@ function destroyBoxes() {
 };
 
 
-createBtn.addEventListener('click', createBoxes);
+createBtn.addEventListener('click', getAmount);
 destroyBtn.addEventListener('click', destroyBoxes);
 
